0cb2db1d5d722cf2be1662d0185799597755d4a8
sbx_user1051 <sbx_user1051@169.254.195.253>
2019-09-14 11:10:29 +0000

    Sat Sep 14 11:10:29 UTC 2019

--- test/solution_tests/HLO/test_hlo.py
+++ test/solution_tests/HLO/test_hlo.py
@@ -1,1 +1,7 @@
-from lib.solutions.SUM import hello_solution
+from lib.solutions.HLO import hello_solution
+
+class TestHelo():
+    def Test_Helo(self):
+        assert hello_solution.hello('pineapple') == 'pineapple'
+
+
