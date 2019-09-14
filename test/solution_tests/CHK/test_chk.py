c6da6e17093def665059635724f91103a61e1bb5
sbx_user1051 <sbx_user1051@169.254.226.21>
2019-09-14 11:31:19 +0000

    Sat Sep 14 11:31:19 UTC 2019

--- test/solution_tests/CHK/test_chk.py
+++ test/solution_tests/CHK/test_chk.py
@@ -6,4 +6,7 @@ class TestCheckout():
         for input in invalid_inputs:
             assert checkout_solution.checkout(input) == -1
 
+    def test_checkout_returns_total_value_for_single_item(self):
+        assert checkout_solution.checkout('A') == 50
+
 
