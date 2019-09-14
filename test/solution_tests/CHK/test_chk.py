af06115ff83d423c7cdb0320f0cd05dc9911bf92
sbx_user1051 <sbx_user1051@169.254.226.21>
2019-09-14 11:25:19 +0000

    Sat Sep 14 11:25:19 UTC 2019

---
+++ test/solution_tests/CHK/test_chk.py
@@ -0,0 +1,8 @@
+from lib.solutions.CHK import checkout_solution
+
+class TestCheckout():
+    def test_checkout_returns_error_when_invalid_input(self):
+        invalid_inputs = ['1', -5, [1,2]]
+        for input in invalid_inputs:
+            assert checkout_solution.checkout(input) == -1
+
