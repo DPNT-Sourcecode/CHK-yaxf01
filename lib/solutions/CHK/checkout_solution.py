719b1aa8e06fa4ae6f74c26f7b6f501d37cebf2f
sbx_user1051 <sbx_user1051@169.254.226.21>
2019-09-14 11:34:19 +0000

    Sat Sep 14 11:34:19 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -6,3 +6,26 @@ def checkout(skus):
     if not isinstance(skus, str):
         return -1
 
+def items = {
+        "A":{
+            "price": 50,
+            "offer" : {
+                "threshold": 3,
+                "price": 130
+            }
+        },
+        "B":{
+            "price": 30,
+            "offer" : {
+                "threshold": 2,
+                "price": 45
+            }
+        },     
+        "C":{
+            "price": 20
+        },
+        "D":{
+            "price": 20
+        },                
+}
+
