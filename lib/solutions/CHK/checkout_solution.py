202a4796f01a07871cfa3cc7aef9559f3b9d2d1b
sbx_user1051 <sbx_user1051@169.254.18.93>
2019-09-18 21:41:36 +0000

    Wed Sep 18 21:41:36 UTC 2019

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -26,7 +26,6 @@ def checkout(skus):
             has_offers = True
             while has_offers:
                 eligible_offers = _get_eligible_offers(available_offers, eligible_items, uitem)
-                print(totalprice)
                 if eligible_offers:
                     saving = _get_highest_saving(eligible_offers)
                     totalprice -= saving['saving']
@@ -73,4 +72,5 @@ def _get_available_offers(item_id):
             available_offers[k] = v
         
     return available_offers
+
 
