da7a9dbdc0f42916500a79808222baa218c05c36
sbx_user1051 <sbx_user1051@169.254.114.133>
2019-09-18 19:36:16 +0000

    Wed Sep 18 19:36:16 UTC 2019

---
+++ lib/solutions/CHK/data/offers.py
@@ -0,0 +1,57 @@
+offers = {
+    0: {
+        'target': {
+            'item': 'A',
+            'quantity': 3
+        },
+        'required': {
+            'item': 'A',
+            'quantity': 3
+        },
+        'saving': 20
+    },
+    1: {
+        'target': {
+            'item': 'A',
+            'quantity': 5
+        },
+        'required': {
+            'item': 'A',
+            'quantity': 5
+        },
+        'saving': 50
+    },
+    2: {
+        'target': {
+            'item': 'B',
+            'quantity': 2
+        },
+        'required': {
+            'item': 'B',
+            'quantity': 2
+        },
+        'saving': 15
+    },
+    3: {
+        'target': {
+            'item': 'B',
+            'quantity': 1
+        },
+        'required': {
+            'item': 'E',
+            'quantity': 2
+        },
+        'saving': 30
+    },
+    4: {
+        'target': {
+            'item': 'F',
+            'quantity': 3
+        },
+        'required': {
+            'item': 'F',
+            'quantity': 3
+        },
+        'saving': 10
+    }
+}
