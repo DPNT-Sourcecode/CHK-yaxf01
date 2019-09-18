4f6181852f30486a973e77bbf48c66cd46e47ddc
sbx_user1051 <sbx_user1051@169.254.243.5>
2019-09-18 20:54:42 +0000

    Wed Sep 18 20:54:42 UTC 2019

--- lib/solutions/CHK/data/items.py
+++ lib/solutions/CHK/data/items.py
@@ -1,22 +1,82 @@
 items_data = {
-        'A':{
-            'price': 50,
-        },
-        'B':{
-            'price': 30,
-        },     
-        'C':{
-            'price': 20
-        },
-        'D':{
-            'price': 15
-        },
-        'E': {
-            'price': 40
-        },
-        'F' : {
-            'price': 10
-        },
-                  
+	'A' : {
+		'price': 
+	}
+	'B' : {
+		'price': 
+	}
+	'C' : {
+		'price': 
+	}
+	'D' : {
+		'price': 
+	}
+	'E' : {
+		'price': 
+	}
+	'F' : {
+		'price': 
+	}
+	'G' : {
+		'price': 
+	}
+	'H' : {
+		'price': 
+	}
+	'I' : {
+		'price': 
+	}
+	'J' : {
+		'price': 
+	}
+	'K' : {
+		'price': 
+	}
+	'L' : {
+		'price': 
+	}
+	'M' : {
+		'price': 
+	}
+	'N' : {
+		'price': 
+	}
+	'O' : {
+		'price': 
+	}
+	'P' : {
+		'price': 
+	}
+	'Q' : {
+		'price': 
+	}
+	'R' : {
+		'price': 
+	}
+	'S' : {
+		'price': 
+	}
+	'T' : {
+		'price': 
+	}
+	'U' : {
+		'price': 
+	}
+	'V' : {
+		'price': 
+	}
+	'W' : {
+		'price': 
+	}
+	'X' : {
+		'price': 
+	}
+	'Y' : {
+		'price': 
+	}
+	'Z' : {
+		'price': 
+	}
 }
+
 
